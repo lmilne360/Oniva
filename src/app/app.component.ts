201539a97e127e1fed0fb83dfbaf649a9d11bb12
Lmilne <lmilne360@gmail.com>
2017-09-10 22:49:39 -0400

    changed  menu home page to tabs page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -22,7 +22,7 @@ export class MyApp {
 
     // used for an example of ngFor and navigation
     this.pages = [
-      { title: 'Home', component: HomePage },
+      { title: 'Home', component: 'TabsPage' },
       { title: 'List', component: ListPage }
     ];
 
